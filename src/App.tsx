0d4de289349717f8471d6d31b3dbf183bc62d79b
Nekoshinji <obito26@gmail.com>
2024-10-30 11:25:05 +0100

    mise en place du route+élément outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 import "./App.css";
 
-// page components
-import Home from "./assets/pages/Home";
-import About from "./assets/pages/About";
-
-
-// the App
-
 function App() {
-  const [currentLocation, setCurrentLocation] = useState("/");
-
   return (
     <>
       <nav>
-        <button onClick={() => setCurrentLocation("/")} type="button">
-          Home
-        </button>
-        <button onClick={() => setCurrentLocation("/about")} type="button">
-          About
-        </button>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
       </nav>
-      <main>
-        {currentLocation === "/" && <Home />}
-        {currentLocation === "/about" && <About />}
-      </main>
+      <main><Outlet /></main>
     </>
   );
 }
 
 export default App;
+
